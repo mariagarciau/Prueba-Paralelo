506ba8225beb23bd23a8db80da93454767a4b0da
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2022-04-27 16:06:42 +0200

    main

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import random
 from time import sleep
 
+urls = ["a.com", "b.com", "c.com", "d.com"]
+
 def scrape(url):
     print("starting", url)
     duration = round(random.random(), 3)
     sleep(duration)
     print("finished", url, "time taken:", duration, "seconds")
     return url, duration
-urls = ["a.com", "b.com", "c.com", "d.com"]output = []
+
+output = []
 for url in urls:
     result = scrape(url)
     output.append(result)
